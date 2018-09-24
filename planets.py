7e7c0cda79718c34c1af6f796f32d417be5100f4
Carlos Garcia-Lemus <garciale@calpoly.edu>
2018-09-24 16:51:12 -0700

    planets.py #stuff

--- planets.py
+++ planets.py
@@ -1,3 +1,14 @@
+#
+# Name: Carlos Garcia-Lemus
+# ID: 014339295
+# Date: 09/24/2018
+# 
+# Lab 0
+# Section 13/14
+# Purpose of lab: Environment, python, and testing
+# Additional comments: 
+
+# Function takes in a number which represents a weight on earth and outputs two numbers which represent that weight being converted to fit Mars’ and Jupiter’s atmospheres.
 def weight_on_planets():
    weight = input("What do you weigh on earth? ")
    MarsW = float(weight) * 0.38
